f7f7406087fb35a362b47011c993441d8c09b112
SumubeiFZ <2210118892@qq.com>
2025-03-12 10:28:27 +0800

    V0

---
+++ Haique_Webots-master/src/ESO/include/eso.h
@@ -0,0 +1,86 @@
+#ifndef __ESO_H__
+#define __ESO_H__
+
+#include "eigen3/Eigen/Eigen"
+#include <iostream>
+#include <ostream>
+#include <vector>
+#include <ros/ros.h>
+#include "ros/publisher.h"
+
+using namespace std;
+
+class ESO{
+    public:
+    ESO(){
+        h = 0.02;
+        z1 = 0.0;
+        z2 = 0.0;
+        e = 0.0; //系统状态误差
+        y = 0.0; //系统输出量
+        fe = 0.0;
+        fe1 = 0.0;
+        beta_01 = 0.4; // 2w
+        beta_02 = 0.04; // w^2
+        u = 0;
+    }
+    ~ESO(){};
+
+    int Sign_ADRC(float Input){
+        int output=0;
+        if(Input>1E-6) output=1;
+        else if(Input<-1E-6) output=-1;
+        else output=0;
+        return output;
+    }
+
+    float Fal_ADRC(float e,float alpha,float zeta){
+        int s=0;
+        float fal_output=0;
+        s=(Sign_ADRC(e+zeta)-Sign_ADRC(e-zeta))/2;
+        fal_output=e*s/(powf(zeta,1-alpha))+powf(fabs(e),alpha)*Sign_ADRC(e)*(1-s);
+        return fal_output;
+    }
+
+    void ESO_ADRC(){
+        e=z1-y; //状态误差
+        fe=Fal_ADRC(e,0.5, h); //非线性函数，提取跟踪状态与当前状态误差
+        fe1=Fal_ADRC(e,0.25,h);
+        
+        /*************扩展状态量更新**********/
+        z1+=h*(z2-beta_01*fe+b0*u);
+        z2+=h*(-beta_02*fe1);
+        // cout << "z1:" << z1 << " z2:" << z2 << endl;
+    }
+
+    void ESOInit(float para1_){
+        b0 = para1_;
+    }
+
+    void SetInput(float input, float state){
+        u = input;
+        y = state;
+    }
+
+    float GetEState(){
+        return z2;
+    }
+
+    private:
+    float h;
+    /*****扩张状态观测器*******/
+    /******已系统输出y和输入u来跟踪估计系统状态和扰动*****/
+    float z1;
+    float z2;
+    float e; //系统状态误差
+    float y; //系统输出量
+    float fe;
+    float fe1;
+    float beta_01;
+    float beta_02;
+    float u;
+    float b0;
+    float d0;
+};
+
+#endif
---
+++ Haique_Webots-master/src/statePublish/src/statePublish_node.cpp
@@ -0,0 +1,165 @@
+#include "ros/console.h"
+#include "ros/publisher.h"
+#include "ros/ros.h"
+#include "ros/service_client.h"
+#include "ros/subscriber.h"
+// for realfly
+// #include <mavros_msgs/CommandBool.h>
+// #include <mavros_msgs/SetMode.h>
+// #include <mavros_msgs/State.h>
+#include <geometry_msgs/PoseStamped.h>
+#include "statePublish/statePub.h"
+#include <gazebo_msgs/ModelStates.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/Imu.h>
+
+#include <webots_ros/set_int.h>
+
+#define TIME_STEP 8  // [ms]
+
+using namespace std;
+
+static statePublish::statePub statePub;
+
+// for realfly
+// mavros_msgs::State current_state;
+
+// for realfly
+// void local_pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+// for sim
+void inertial_unitCallback(const sensor_msgs::Imu::ConstPtr& msg){
+    double roll, pitch, yaw;
+
+    // for realfly
+    // geometry_msgs::PoseStamped current_odom = *msg;
+    // for sim
+    geometry_msgs::Quaternion model_orientation = msg->orientation;
+    geometry_msgs::Twist model_twist;
+    model_twist.angular = msg->angular_velocity;
+
+    // for realfly
+    // double w = current_odom.pose.orientation.w;
+    // double x = current_odom.pose.orientation.x;
+    // double y = current_odom.pose.orientation.y;
+    // double z = current_odom.pose.orientation.z;
+    // for sim
+    double w = model_orientation.w;
+    double x = model_orientation.x;
+    double y = model_orientation.y;
+    double z = model_orientation.z;
+
+    double sinr_cosp = 2 * (w * x + y * z);
+    double cosr_cosp = 1 - 2 * (x * x + y * y);
+    roll = atan2(sinr_cosp, cosr_cosp);
+
+    double sinp = 2 * (w * y - z * x);
+    if (abs(sinp) >= 1)
+        pitch = std::copysign(M_PI / 2, sinp); // 使用 90 度或 -90 度
+    else
+        pitch = asin(sinp);
+
+    double siny_cosp = 2 * (w * z + x * y);
+    double cosy_cosp = 1 - 2 * (y * y + z * z);
+    yaw = atan2(siny_cosp, cosy_cosp);
+    statePub.roll = roll;
+    statePub.pitch = pitch;
+    statePub.yaw = yaw;
+    cout << "roll: " << roll << " pitch: " << pitch << " yaw: " << yaw << endl;
+}
+
+void GyroCallback(const sensor_msgs::Imu::ConstPtr& msg){
+    geometry_msgs::Twist model_twist;
+    model_twist.angular = msg->angular_velocity;
+    double w_x = model_twist.angular.x;
+    double w_y = model_twist.angular.y;
+    double w_z = model_twist.angular.z;
+    statePub.angular_velocity_x = w_x;
+    statePub.angular_velocity_y = w_y;
+    statePub.angular_velocity_z = w_z;
+}
+
+void AccCallback(const sensor_msgs::Imu::ConstPtr& msg){
+    geometry_msgs::Twist model_twist;
+    model_twist.linear = msg->linear_acceleration;
+    double v_x = model_twist.linear.x;
+    double v_y = model_twist.linear.y;
+    double v_z = model_twist.linear.z;
+    statePub.linear_acceleration_x = v_x;
+    statePub.linear_acceleration_y = v_y;
+    statePub.linear_acceleration_z = v_z;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "main");
+    ros::NodeHandle nh;
+    
+    // Wait for the `ros` controller.
+    ros::service::waitForService("/robot/time_step");
+    ros::spinOnce();
+    ros::Duration(1).sleep();
+
+    ROS_INFO("state publish initialized.");
+    // for realfly
+    // ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    // ros::Subscriber local_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>(
+    //                                 "mavros/local_position/pose", 10, local_pose_cb);
+    // for sim
+    ros::ServiceClient inertial_unit_Client = nh.serviceClient<webots_ros::set_int>("/inertial_unit/enable");
+    ros::Subscriber inertial_unit_sub = nh.subscribe("/inertial_unit/quaternion", 10, inertial_unitCallback);
+    ros::ServiceClient Gyro_Client = nh.serviceClient<webots_ros::set_int>("/gyro/enable");
+    ros::Subscriber Gyro_sub = nh.subscribe("/gyro/values", 10, GyroCallback);
+    ros::ServiceClient accelerometer_Client = nh.serviceClient<webots_ros::set_int>("/accelerometer/enable");
+    ros::Subscriber accelerometer_sub = nh.subscribe("/accelerometer/values", 10, AccCallback);
+    ros::Publisher statePublisher = nh.advertise<statePublish::statePub>("/statePub", 10);
+
+    // initialize the statePub
+    statePub.roll = 0;
+    statePub.pitch = 0;
+    statePub.yaw = 0;
+    statePub.angular_velocity_x = 0;
+    statePub.angular_velocity_y = 0;
+    statePub.angular_velocity_z = 0;
+
+    webots_ros::set_int inertial_unit_Srv;     
+    inertial_unit_Srv.request.value = TIME_STEP;
+    if (inertial_unit_Client.call(inertial_unit_Srv) && inertial_unit_Srv.response.success) {
+        ROS_INFO("imu enabled.");
+    } else {
+        if (!inertial_unit_Srv.response.success)
+        ROS_ERROR("Failed to enable imu.");
+        return 1;
+    }
+
+    webots_ros::set_int gyro_Srv;
+    gyro_Srv.request.value = TIME_STEP;
+    if (Gyro_Client.call(gyro_Srv) && gyro_Srv.response.success) {
+        ROS_INFO("gyro enabled.");
+    } else {
+        if (!gyro_Srv.response.success)
+        ROS_ERROR("Failed to enable gyro.");
+        return 1;
+    }
+
+    webots_ros::set_int acc_Srv;
+    acc_Srv.request.value = TIME_STEP;
+    if (accelerometer_Client.call(acc_Srv) && acc_Srv.response.success) {
+        ROS_INFO("accelerometer enabled.");
+    } else {
+        if (!acc_Srv.response.success)
+        ROS_ERROR("Failed to enable accelerometer.");
+        return 1;
+    }
+
+    ros::Rate rate(50.0);
+
+    while(ros::ok()) {
+        statePublisher.publish(statePub);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+}
